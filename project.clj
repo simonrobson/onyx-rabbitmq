d51ce0323aa59fbd8ef886fb9b5b35aac57f6b1a
Simon Robson <shrobson@gmail.com>
2015-09-15 15:13:48 +0700

    bump onyx version

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.onyxplatform/onyx "0.7.0"]
+                 [org.onyxplatform/onyx "0.7.3"]
                  [com.novemberain/langohr "3.3.0"]
                  [less-awful-ssl "1.0.0"]
                  [environ "1.0.0"]]
